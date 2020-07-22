b1793247d39e963a35ecc3f438162b247b994d30
Cameron Lofy <cameron.lofy@sjsu.edu>
2020-07-22 16:03:29 -0700

    adjusting gitignore stuff

--- ip_notification/config.py
+++ ip_notification/config.py
@@ -1,4 +1,5 @@
 # Place IP addresses here in this format
 ip_list = {"192.168.86.1":"Cameron's Phone",
 		   "192.168.86.2":"Jon's Phone",
-		   "192.168.86.2":"Cynthia's Phone"}
+		   "192.168.86.2":"Cynthia's Phone",
+		   "1.1.1.1":"example"}
