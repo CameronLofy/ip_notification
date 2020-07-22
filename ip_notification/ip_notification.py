c78718da574455b732d041772e32b96cc39bd442
Cameron Lofy <cameron.lofy@sjsu.edu>
2020-07-22 16:38:57 -0700

    fixed functionality for multiple IPs

--- ip_notification/ip_notification.py
+++ ip_notification/ip_notification.py
@@ -6,18 +6,22 @@ from config import ip_list
 
 #IP_DEVICE = config('IP_DEVICE')
 hostnames = ip_list
-online = False
+notified = {}
+for ip in hostnames:
+    notified.update({ip:False})
+
+
 while True:
     for ip in hostnames:
         output = subprocess.Popen(["ping", ip],stdout = subprocess.PIPE).communicate()[0]
         #print(output)
         str_output = str(output, 'utf-8')
         if ("Destination host unreachable" in str_output):
-            if online == True:
-                print("Offline")
-            online = False
+            if notified[ip] == True:
+                print(hostnames[ip]," is offline")
+            notified[ip] = False
         else:
-            if online == False:
+            if notified[ip] == False:
                 send_txt(hostnames[ip])
                 print(hostnames[ip]," is online")
-            online = True
+            notified[ip] = True
