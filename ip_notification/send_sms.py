108eda05c18fbc5a6ba02a193215ab239e2bf478
Cameron Lofy <cameron.lofy@sjsu.edu>
2020-07-22 16:14:31 -0700

    added ignored values to send_sms.py

--- ip_notification/send_sms.py
+++ ip_notification/send_sms.py
@@ -1,13 +1,13 @@
 # we import the Twilio client from the dependency we just installed
 from twilio.rest import Client
-
+from config import *
 # the following line needs your Twilio Account SID and Auth Token
-client = Client("Insert Account SID Here", "insert Auth Token Here")
+client = Client(twilio_account, twilio_auth)
 
 # change the "from_" number to your Twilio number and the "to" number
 # to the phone number you signed up for Twilio with, or upgrade your
 # account to send SMS to any phone number
 def send_txt(name):
-    client.messages.create(to="+1XXXXXXXXXX",
-                           from_="+1XXXXXXXXXX",
+    client.messages.create(to=twilio_num,
+                           from_=user_num,
                            body= name + "is online")
