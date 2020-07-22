5c15ee240dba115044685921bf6d06535b0f5d98
Cameron Lofy <cameron.lofy@sjsu.edu>
2020-07-22 16:51:02 -0700

    added more comments for clarity

--- ip_notification/ip_notification.py
+++ ip_notification/ip_notification.py
@@ -11,17 +11,23 @@ for ip in hostnames:
     notified.update({ip:False})
 
 
+# Forever looping to check which IPs are connected to wifi
 while True:
+
+    # Looing through list of IPs to check
     for ip in hostnames:
         output = subprocess.Popen(["ping", ip],stdout = subprocess.PIPE).communicate()[0]
-        #print(output)
         str_output = str(output, 'utf-8')
+
+        # If IP is offline, the output will have "Destinatino host unreachable"
         if ("Destination host unreachable" in str_output):
             if notified[ip] == True:
                 print(hostnames[ip]," is offline")
             notified[ip] = False
+
         else:
             if notified[ip] == False:
+                # Using the send_sms function in send_sms.py
                 send_txt(hostnames[ip])
                 print(hostnames[ip]," is online")
             notified[ip] = True
--- ip_notification/send_sms.py
+++ ip_notification/send_sms.py
@@ -1,13 +1,37 @@
-# we import the Twilio client from the dependency we just installed
 from twilio.rest import Client
 from config import *
+
 # the following line needs your Twilio Account SID and Auth Token
 client = Client(twilio_account, twilio_auth)
 
-# change the "from_" number to your Twilio number and the "to" number
-# to the phone number you signed up for Twilio with, or upgrade your
-# account to send SMS to any phone number
+# to fill the twilio_account, twilio_auth, twilio_num, and user_num
+# create a file called config.py with the values of these variables
+# as shown below in this config.py format:
+
+""" 
+# config.py
+# Place IP addresses here in this format
+ip_list = {"192.168.86.22":"Cameron's Phone",
+		   "192.168.86.2":"Jon's Phone",
+		   "192.168.86.3":"Cynthia's Phone"}
+
+
+twilio_account = "ACxxxxxxxxxxxxxxxxx"
+twilio_auth = "xxxxxxxxxxxxxxxxxxxxx"
+
+twilio_num = "+1XXXXXXXXXX"
+user_num = "+1XXXXXXXXXX"
+
+"""
+
+
+
+
 def send_txt(name):
     client.messages.create(to=twilio_num,
                            from_=user_num,
                            body= name + "is online")
+
+
+
+#
